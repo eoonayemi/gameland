163f846188f18781a67fa0d1a542a26cb7fe36ed
Emmanuel <eoonayemi@gmail.com>
2024-10-01 15:45:40 +0100

    Adjusted sign up for update box for proper space on mobile

--- src/components/shared/ContactCard.tsx
+++ src/components/shared/ContactCard.tsx
@@ -5,17 +5,19 @@ interface ContactCardProps {
   title: string;
   titleStyles: string;
   description?: string;
+  boxStyles?: string;
   children?: React.ReactNode;
 }
 
 const ContactCard: React.FC<ContactCardProps> = ({
   title,
   titleStyles,
+  boxStyles,
   description,
   children,
 }) => {
   return (
-    <div>
+    <div className={clsx(boxStyles, "overflow-hidden flex-1")}>
       <h2 className={clsx(titleStyles, "text-lg mb-2")}>{title}</h2>
       {description && <p className="text-gray-600">{description}</p>}
       {children && children}
--- src/components/shared/InputWithBtn.tsx
+++ src/components/shared/InputWithBtn.tsx
@@ -21,20 +21,21 @@ const InputWithBtn: React.FC<InputWithBtnProps> = ({
     <div
       className={clsx(
         boxStyles,
-        "bg-[#262626] flex items-center gap-5 p-2 rounded-lg w-fit"
+        "bg-[#262626] flex gap-5 justify-between items-center p-2 rounded-lg overflow-hidden"
       )}
     >
       <input
         type={type}
         placeholder={placeholder}
-        className="bg-transparent outline-none placeholder:text-gray-600 px-2"
+        className="bg-transparent {border border-fuchsia-500} outline-none placeholder:text-gray-600 px-2 w-[60%]"
       />
+
       <CustomButton
         tagName={btnTag}
         styles={`${
           btnStyles
             ? btnStyles
-            : "bg-gradient-to-b from-secondary to-primary text-gray-800 font-semibold"
+            : "bg-gradient-to-b from-secondary to-primary text-gray-800 font-semibold w-[40%] flex justify-center items-center"
         }`}
       />
     </div>
